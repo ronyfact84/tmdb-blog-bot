1bccd655f03172dca3be3c32f7f501e3ec83c7ae
ronyfact84 <ronyfact84@gmail.com>
2026-06-13 17:26:13 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,41 @@
-
 import requests
+import os
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 
-TMDB_API_KEY = "7472337f81f4ea2baea17e6253bc1077"
-BLOG_ID = "4894620542423770622"
+TMDB_API_KEY = os.environ["TMDB_API_KEY"]
+BLOG_ID = os.environ["BLOG_ID"]
 
-# GET POPULAR MOVIE
-def get_movie():
+def get_movies():
     url = f"https://api.themoviedb.org/3/movie/popular?api_key={TMDB_API_KEY}"
-    data = requests.get(url).json()
-    return data["results"][0]
+    return requests.get(url).json()["results"]
 
-# GET TRAILER
-def get_trailer(movie_id):
-    url = f"https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key={TMDB_API_KEY}"
-    data = requests.get(url).json()
-
-    for v in data.get("results", []):
-        if v["site"] == "YouTube" and v["type"] == "Trailer":
-            return v["key"]
-    return None
-
-# BUILD POST
-def build_post(movie, trailer):
-    title = movie["title"]
-    poster = "https://image.tmdb.org/t/p/w500" + movie["poster_path"]
-
-    content = f"""
-    <h1>{title}</h1>
-    <img src="{poster}" width="300"/>
-
-    <p>{movie.get('overview','')}</p>
-
-    <h3>Trailer</h3>
-    <iframe width="100%" height="450"
-    src="https://www.youtube.com/embed/{trailer}"
-    frameborder="0" allowfullscreen></iframe>
-    """
-
-    return title, content
-
-# POST TO BLOGGER
 def post_to_blogger(title, content):
-    service = build("blogger", "v3", developerKey=TMDB_API_KEY)
+
+    creds = Credentials(
+        None,
+        refresh_token=os.environ["REFRESH_TOKEN"],
+        token_uri="https://oauth2.googleapis.com/token",
+        client_id=os.environ["CLIENT_ID"],
+        client_secret=os.environ["CLIENT_SECRET"]
+    )
+
+    creds.refresh(Request())
+
+    service = build("blogger", "v3", credentials=creds)
 
     service.posts().insert(
         blogId=BLOG_ID,
-        body={"title": title, "content": content}
+        body={
+            "title": title,
+            "content": content
+        }
     ).execute()
 
-    print("Posted Successfully!")
+movies = get_movies()
 
-# MAIN
-movie = get_movie()
-trailer = get_trailer(movie["id"])
-
-title, content = build_post(movie, trailer)
-post_to_blogger(title, content)
+for m in movies[:3]:
+    title = m["title"]
+    content = f"<img src='https://image.tmdb.org/t/p/w500{m['poster_path']}'><br>{m['overview']}"
+    post_to_blogger(title, content)
