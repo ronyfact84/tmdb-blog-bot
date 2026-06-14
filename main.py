3d95dee32651857f95912393b4efbde17b5ee932
ronyfact84 <ronyfact84@gmail.com>
2026-06-14 14:59:57 +0600

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ BLOG_ID = os.environ["BLOG_ID"]
 
 def get_movies():
     url = f"https://api.themoviedb.org/3/movie/popular?api_key={TMDB_API_KEY}"
-    return requests.get(url).json()["results"]
+    data = requests.get(url).json()
+    return data.get("results", [])
 
 def post_to_blogger(title, content):
-
     creds = Credentials(
         None,
         refresh_token=os.environ["REFRESH_TOKEN"],
@@ -36,6 +36,20 @@ def post_to_blogger(title, content):
 movies = get_movies()
 
 for m in movies[:3]:
-    title = m["title"]
-    content = f"<img src='https://image.tmdb.org/t/p/w500{m['poster_path']}'><br>{m['overview']}"
-    post_to_blogger(title, content)
+    title = m.get("title", "No Title")
+
+    poster = m.get("poster_path")
+    if poster:
+        img = f"<img src='https://image.tmdb.org/t/p/w500{poster}'>"
+    else:
+        img = ""
+
+    overview = m.get("overview", "")
+
+    content = f"{img}<br>{overview}"
+
+    try:
+        post_to_blogger(title, content)
+        print("Posted:", title)
+    except Exception as e:
+        print("Error posting:", title, e)
