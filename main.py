c8b573bc59ac6bc27cbb63908c7a5d21fa19a17f
ronyfact84 <ronyfact84@gmail.com>
2026-06-15 09:58:45 +0600

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,12 @@ def trailer_system(title):
     <div onclick="openTrailer()"
     style="
         width:100%;
-        height:220px;
+        padding:20px;
         background:#111;
         color:white;
-        display:flex;
-        align-items:center;
-        justify-content:center;
+        text-align:center;
         border-radius:10px;
         cursor:pointer;
-        font-size:18px;
         font-weight:bold;">
         ▶ Click to Watch Trailer
     </div>
@@ -24,32 +21,25 @@ def trailer_system(title):
         display:none;
         position:fixed;
         top:0;left:0;
-        width:100%;height:100%;
+        width:100%;
+        height:100%;
         background:rgba(0,0,0,0.95);
         z-index:9999;
         justify-content:center;
         align-items:center;">
 
-        <div style="width:90%;max-width:900px;position:relative;">
+        <div style="width:90%;max-width:900px;">
 
             <iframe width="100%" height="500"
             src="{search_url}"
-            style="border:none;background:white;">
+            style="border:none;">
             </iframe>
 
             <button onclick="closeTrailer()"
-            style="
-                position:absolute;
-                top:-20px;
-                right:-20px;
-                background:white;
-                border:none;
-                font-size:22px;
-                width:40px;
-                height:40px;
-                border-radius:50%;
-                cursor:pointer;">
-                ✖
+            style="position:absolute;top:20px;right:20px;
+            background:white;border:none;
+            width:40px;height:40px;border-radius:50%;">
+            ✖
             </button>
 
         </div>
@@ -57,10 +47,10 @@ def trailer_system(title):
 
     <script>
     function openTrailer() {{
-        document.getElementById("popup").style.display = "flex";
+        document.getElementById("popup").style.display="flex";
     }}
     function closeTrailer() {{
-        document.getElementById("popup").style.display = "none";
+        document.getElementById("popup").style.display="none";
     }}
     </script>
 
